8c0f482e530b62a0bb5f021c53f72ce23c3c5117
blastmaster <oeste.sebastian@googlemail.com>
2012-11-23 05:45:55 -0500

    little steps
    * welcome site with link and button to increment the beer counter
    * login site with no user management at present
    * stats site which momentary just print your beer counter
    
    TODO
    * storing the current stats
    * session management
    * authentification must be improved

--- lib/BeerPlusPlus.pm
+++ lib/BeerPlusPlus.pm
@@ -4,17 +4,28 @@ use feature "say";
 package BeerPlusPlus;
 
 use Mojolicious::Lite;
-plugin "TagHelpers";
 
-# ABSTRACT:
+# ABSTRACT: first steps to an application which manage the beer storage
 
 get '/' => 'index';
+
+get '/statistics' => sub {
+    my $self = shift;
+    $self->render(controller => 'statistics');
+};
 
 post '/welcome' => sub {
     my $self = shift;
     my $user = $self->param('user');
+    $self->session->{user} = $user;
     my $pass = $self->param('pass');
-    $self->render(text => "$user $pass");
+    $self->render(controller => 'welcome');
+};
+
+post '/increment' => sub {
+    my $self = shift;
+    $self->session->{counter}++;
+    $self->render('welcome');
 };
 
 app->start;
