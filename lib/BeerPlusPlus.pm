f69e3d3f2860cc83cbbf48c7ce08643ef6cee7ba
8ware <8wared@googlemail.com>
2013-10-02 15:28:57 +0200

    improved beer++ application and added CSS
    * added denied-page and show it when authentication fails
    * improved statistics-page by showing statistics of all users
    * added logout-page and choose randomly byebye-slogan
    * added basic-layout and adjusted all pages
    * some little improvements, e.g. argument checking, error messages etc.

--- lib/BeerPlusPlus.pm
+++ lib/BeerPlusPlus.pm
@@ -45,8 +45,9 @@ helper init => sub {
 helper json2hash => sub {
     my $self = shift;
     my $filename = shift;
-    # put more error handling instead of just dying
-    open my $fh, '<', 'foo/'.$filename.'.json' or die qq/cannot open $!/;
+	my $path = "foo/$filename.json";
+    # TODO put more error handling instead of just dying
+    open my $fh, '<', $path or die qq/cannot open $path: $!/;
     $/ = undef;
     my $data = <$fh>;
     close $fh;
@@ -65,6 +66,7 @@ helper get_users => sub {
 
 helper user_exists => sub {
     my ($self, $username) = @_;
+	return 0 unless $username;
     my %users = map { $_ => 1 } ( $self->get_users );
     return 1 if (exists $users{$username});
     return 0;
@@ -105,13 +107,29 @@ get '/' => 'index';
 under sub {
     my $self = shift;
     return 1 if $self->auth;
-    $self->render(text => "denied<br/>Rin\'tel\'noc!");
-    return 0;
+	$self->render('denied', subtitle => "rin'tel'noc");
+	return 0;
+};
+
+get '/denied' => sub {
+	my $self = shift;
+	$self->render(controller => 'denied', subtitle => "rin'tel'noc");
 };
 
 get '/statistics' => sub {
     my $self = shift;
-    $self->render(controller => 'statistics');
+	my $user = $self->session->{user};
+	my %statistics;
+	for my $userfile (glob "foo/*.json") {
+		my ($name) = $userfile =~ /foo\/(.+)\.json$/;
+		next if $name eq $user;
+
+		open FILE, '<', $userfile or die $!;
+		my $hash = $self->json2hash($name);
+		close FILE or warn $!;
+		$statistics{$name} = $hash->{counter};
+	}
+    $self->render(controller => 'statistics', stats => \%statistics);
 };
 
 get '/logout' => sub {
@@ -120,7 +138,10 @@ get '/logout' => sub {
     delete $self->session->{expected_pass};
     %{ $self->session } = ();
     $self->session(expires => 1);
-    $self->render(text => 'kree sha!<br/>logging out ...');
+	my @byebyes = ('kree sha', 'lek tol');
+	my $byebye = $byebyes[rand @byebyes];
+#	$self->render(text => "$byebye!<br/>logging out ...");
+	$self->render(controller => 'logout', byebye => $byebye, subtitle => $byebye);
 };
 
 get '/chpw' => sub { shift->render('register'); };
@@ -150,19 +171,36 @@ app->start;
 __DATA__
 
 @@ index.html.ep
-<!DOCTYPE html>
-<html>
-<head>
-<title>BeerPlusPlus</title>
-</head>
-<body>
-<div id=1>
+% layout 'basic', subtitle => 'increment your blood alcohol level';
 %=form_for '/login' => (method => 'POST') => begin
-%=text_field 'user'
-%=password_field 'pass'
-%=submit_button 'login', id => 'login'
-%end
+	<table>
+		<tr>
+			<td class="banner">beer</td>
+		</tr>
+		<tr>
+			<td><%=text_field 'user', id => 'user' %></td>
+			<td><%=password_field 'pass', id => 'pass' %></td>
+		</tr>
+		<tr>
+			<td/>
+			<td><%=submit_button '++', id => 'login', class => 'banner', title => 'login' %></td>
+		</tr>
+	</table>
+%=end
+
+@@ logout.html.ep
+% layout 'basic';
+<p class="banner center"><%= $byebye %>!</p>
+<div id="footer">
+	<span style="float: right"><%=link_to 'login' => '/' %> |</span>
 </div>
-</body>
-</html>
+
+@@ denied.html.ep
+% layout 'basic';
+<span class="banner center" style="font-size: 3.7em">Rin'tel'noc!</span>
+<br/>
+Permission denied!
+<div id="footer">
+	<span style="float: right"><%=link_to 'login' => '/' %> |</span>
+</div>
 
