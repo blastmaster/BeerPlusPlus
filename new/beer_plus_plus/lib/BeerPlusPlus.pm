3a3b71e9924593cd5bf258b41e2d461e4552907a
blastmaster <blastmaster@tuxcode.org>
2013-10-19 21:40:49 +0200

    first stage of controller who manage the routing

--- new/beer_plus_plus/lib/BeerPlusPlus.pm
+++ new/beer_plus_plus/lib/BeerPlusPlus.pm
@@ -1,18 +1,71 @@
 package BeerPlusPlus;
 use Mojo::Base 'Mojolicious';
 
+use BeerPlusPlus::User;
+
+use Data::Printer;
+use feature "say";
+
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+	my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+	# Documentation browser under "/perldoc"
+	# $self->plugin('PODRenderer');
 
-  # Router
-  my $r = $self->routes;
+	# create user object
+	$self->helper(user => sub { state $user = BeerPlusPlus::User->new });
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+	# Router
+	my $r = $self->routes;
+
+	$r->get( '/register' => sub {
+		my $self = shift;
+		$self->render('register');
+	});
+
+	$r->any('/')->to('login#login');
+	$r->post('/login')->to('login#index');
+	$r->get('/logout')->to('login#logout');
+
+	$r = $r->under->to('login#is_auth');
+
+	$r->get('/welcome' => sub { shift->render(template => 'welcome', format => 'html'); });
+
+	$r->post('/increment' => sub {
+		my $self = shift;
+		p $self;
+		$self->session->{counter}++;
+		$self->user->persist;
+		$self->redirect_to('/welcome');
+	});
+
+	$r = $r->under(sub {
+			my $self = shift;
+			$self->render(controller => 'denied', subtitle => "rin'tel'noc");
+			return 0;
+	});
+
+	# $r = $r->add_condition('check' => sub {
+	# 	my $self = shift;
+	# 	return 1 if $self->auth;
+	# 	$self->render('denied', subtitle => "rin'tel'noc");
+	# 	return 0;
+	# });
+
+	$r->get('/denied' => sub {
+		my $self = shift;
+		$self->render(controller => 'denied', subtitle => "rin'tel'noc");
+	});
+
+
+	$r->get('/chpw' => sub { shift->render('register'); });
+
+
+
+
+	# Normal route to controller
+	# $r->get('/')->to('example#welcome');
 }
 
 1;
