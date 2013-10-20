11168f809904c22122b92cec90d40f5f01fd0e64
blastmaster <blastmaster@tuxcode.org>
2013-10-20 21:55:25 +0200

    adjust statistics view to new user file structure, also not perfect at the time, but much better then before

--- lib/BeerPlusPlus/Statistics.pm
+++ lib/BeerPlusPlus/Statistics.pm
@@ -1,27 +1,21 @@
 package BeerPlusPlus::Statistics;
 use Mojo::Base 'Mojolicious::Controller';
 
-#FIXME second time same variable, get rid of this and use config
-my $DATADIR = 'users';
-
 sub statistics
 {
 	my $self = shift;
-	my $user = $self->session->{user};
 
 	my %statistics;
-	for my $userfile (glob "$DATADIR/*.json") {
-		my ($name) = $userfile =~ /$DATADIR\/(.+)\.json$/;
-		next if $name eq $user;
+    my @otherusers = $self->user->get_others();
 
-		open FILE, '<', $userfile or die $!;
-		my $hash = $self->user->json2hash($name);
-		close FILE or warn $!;
+    for my $userhash (@otherusers) {
+        # hash accesses will be replaced by suitable method call's
+        my $name = $userhash->{user};
+        my $count = @{$userhash->{times}};
+        $statistics{$name} = $count if $count;
+    }
 
-		my $count = $hash->{counter};
-		$statistics{$name} = $count if $count;
-	}
-	$self->render(template => 'statistics', stats => \%statistics);
+    $self->render(template => 'statistics', stats => \%statistics);
 }
 
 1;
