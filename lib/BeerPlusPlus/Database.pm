1acdced5328be1ce4bc2f93fff303dcb91a786f2
8ware <8wared@googlemail.com>
2013-12-20 00:13:01 +0100

    corrected copyright to Innercircle

--- lib/BeerPlusPlus/Database.pm
+++ lib/BeerPlusPlus/Database.pm
@@ -211,7 +211,7 @@ __END__
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013 by 8ware
+Copyright (C) 2013 by Innercircle
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
