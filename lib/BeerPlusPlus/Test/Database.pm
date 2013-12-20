4390df0eeeafe9f44e8ce8d7a2ef226ebfdc6397
8ware <8wared@googlemail.com>
2013-12-20 15:56:33 +0100

    improved database test module
    * moved setting of datadir to separate subroutine so it can be
      called/reset several times during the test
    * added POD regarding the author and licence

--- lib/BeerPlusPlus/Test/Database.pm
+++ lib/BeerPlusPlus/Test/Database.pm
@@ -24,6 +24,7 @@ database module internally
 
 =head1 DESCRIPTION
 
+Sets the database module's variable C<DATADIR> to a temporary directory.
 This module is intended for test purposes only since the data get lost
 after the application terminated.
 
@@ -35,6 +36,45 @@ use File::Basename;
 use File::Temp 'tempdir';
 
 
-$BeerPlusPlus::Database::DATADIR
-		= tempdir(dirname($0) . '/db.XXXXXXX', CLEANUP => 1);
+=head2 SUBROUTINES
+
+=over 4
+
+=item reset_datadir()
+
+Sets the database module's C<DATADIR> variable to a temporary directory
+which is removed after the test terminated. This subroutine is called once
+during this module is loaded.
+
+=cut
+
+sub reset_datadir() {
+	my $template = dirname($0) . '/db.XXXXXXX';
+	$BeerPlusPlus::Database::DATADIR = tempdir($template, CLEANUP => 1);
+}
+
+=back
+
+=cut
+
+
+reset_datadir();
+
+
+1;
+__END__
+
+=head1 AUTHOR
+
+8ware, E<lt>8wared@googlemail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Innercircle
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
 
